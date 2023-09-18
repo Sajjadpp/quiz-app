79309ec5c02674022815692f69f98b1267613243
sajjad <sajjadmuhammed227@gmail.com>
2023-09-18 07:09:28 -0700

    1st commit of quiz app

---
+++ script.js
@@ -0,0 +1,42 @@
+const questions =[
+    {
+        question: 'What is the name of this former soccer player from France?',
+        answers:[
+        {text: 'Wayne Rooney',correct: false},
+        {text: 'Andreas Iniesta',correct: false},
+        {text: 'Arjen Robben',correct: false},
+        {text: 'Zinedine Zidane',correct: true},
+        
+        ] 
+    },
+    {
+        question: 'What is the name of this player?',
+        answers:[
+        {text: 'Francesco Totti',correct: true},
+        {text: 'Xavi',correct: false},
+        {text: 'Kaka',correct: false},
+        {text: 'Paolo Maldini',correct: false},
+        
+        ] 
+    },
+    {
+        question: 'He plays for A.C. Milan. What is his name?',
+        answers:[
+        {text: 'Robin Van Persie',correct: false},
+        {text: 'Mario Balotelli',correct: true},
+        {text: 'Didier Drogba Robben',correct: false},
+        {text: 'Roberto Carlos',correct: false},
+        
+        ] 
+    },
+    {
+        question: 'Whats the name of this former soccer player from France?',
+        answers:[
+        {text: 'David Beckham',correct: true},
+        {text: 'Zlatan Ibrahimović',correct: false},
+        {text: 'Sergio Aguero',correct: false},
+        {text: 'Steven Gerrard',correct: false},
+        
+        ] 
+    },
+]
